a775b47de1d1dc39b777bbf5d6efc3c70f261ed7
T0niTruum <mjeankenneth@gmail.com>
2025-05-24 18:45:49 -0500

    Crear usuario

--- Servidor.ex
+++ Servidor.ex
@@ -8,4 +8,19 @@ defmodule Proyecto.Servidor do
       mensajes: %{"general" => []} # %{sala => [mensajes]}
     }, name: {:global, __MODULE__})
   end
+
+  def init(state), do: {:ok,state}
+
+  #Conectar un usuario
+  def handle_call({:connect, nombre, pid}, _from, state) do
+    if Map.has_key?(state.usuarios, nombre) do
+      {:reply, {:error, "Usuario ya conectado"}, state}
+    else
+      usuarios = Map.put(state.usuarios, nombre, %{pid: pid, sala: "general"})
+      salas = Map.update!(state.salas, "general", fn usuarios -> [nombre | usuarios] end)
+      {:reply, :ok, %{sate | usuarios: usuarios, salas: salas}}
+    end
+  end
+
+  
 end
